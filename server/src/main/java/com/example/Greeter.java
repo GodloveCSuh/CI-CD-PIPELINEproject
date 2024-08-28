a3a1c8b62072cce89607c7af9e8e0952ac6134dc
Godlove s <schongwain@gmail.com>
2024-08-28 04:03:05 -0400

    trial

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,6 +16,7 @@ public class Greeter {
    * This is a method.
    */
   public final String greet(final String someone) {
-    return String.format("Hello Avinash, %s!", someone);
+    String password = "admin@123";
+    return String.format("Hello GoddySuh, %s!", someone);
   }
 }
